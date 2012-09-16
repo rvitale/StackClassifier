536257274aa7110c4d6a63772484b3e7e9dd0a51
libra guest <ric.vitale@gmail.com>
2012-09-16 11:54:48 -0400

    Modified the new Bayes classifier in order to take into account different kind of records and separate the extracted features.

--- src/main/java/com/stackoverflow/NaiveBayesClassifierNumeric.java
+++ src/main/java/com/stackoverflow/NaiveBayesClassifierNumeric.java
@@ -3,6 +3,9 @@ package com.stackoverflow;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 import com.stackoverflow.utils.CsvReader;
 
@@ -11,35 +14,51 @@ public class NaiveBayesClassifierNumeric {
     private String[] featureNames;
     private String parameterName;
     
-    protected double[] means, variances;
+    protected Map<String, Map<String, Double>> means, variances;
     
     public NaiveBayesClassifierNumeric(String[] features, String parameter) {
         featureNames = features;
         parameterName = parameter;
         
-        means = new double[features.length];
-        variances = new double[features.length];
+        means = new HashMap<String, Map<String, Double>>();
+        variances = new HashMap<String, Map<String, Double>>();
     }
     
     public void train(File trainingFile) throws IOException {
         CsvReader reader = new CsvReader(trainingFile);
         int counter = 0;
         for (Map<String, String> line : reader) {
-            for (int i = 0; i < featureNames.length; ++i) {
-                String featureName = featureNames[i];
+            for (String featureName : featureNames) {
                 
                 if(!line.containsKey(featureName)) {
                     //TODO: Better error message
                     throw new IOException("Training File does not contain expected data");
                 }
                 double feature = Double.parseDouble(line.get(featureName));
+                String parameter = line.get(parameterName);
                 
-                // avg = (avg(n-1) * (n -1)) + Xn / n               abg = sum(Xi) / n | i=1..n
-                means[i] = (means[i] * counter + feature) / (counter + 1);
+                Map<String, Double> means;
+                if(!means.containsKey(parameter)) {
+                    means = newMeansMap();
+                    means.put(parameter, means);   
+                } else {
+                    means = means.get(parameter);
+                }
+                // avg = (avg(n-1) * (n -1)) + Xn / n               avg = sum(Xi) / n | i=1..n
+                double newMean = (means.get(featureName) * counter + feature) / (counter + 1);
+                means.put(featureName, newMean);
             }
             ++counter;
         }
     }
     
-    
+    private Map<String, Double> newMeansMap() {
+        
+        Map<String, Double> means = new HashMap<String, Double>();
+        for(String featureName : featureNames) {
+            means.put(featureName, 0.0);   
+        }
+        
+        return means;
+    }
 }
